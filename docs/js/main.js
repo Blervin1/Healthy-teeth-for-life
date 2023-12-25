17eae1d25034e7f8d1c7df1210437edb80903aff
Blervin <126615593+Blervin1@users.noreply.github.com>
2023-12-25 03:08:54 +0200

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,72 @@
+// Mobile Menu Burger
+function burgerMenu() {
+	const menu = document.querySelector('#mobile-menu')
+	const burger = document.querySelector('#burger')
+	const body = document.querySelector('body')
+
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('active')
+		menu.classList.toggle('hidden')
+		menu.classList.toggle('flex')
+		body.classList.toggle('overflow-hidden')
+	})
+
+	// Вот тут мы ставим брейкпоинт навбара
+	window.addEventListener('resize', () => {
+		if (window.innerWidth > 991.98) {
+			menu.classList.add('hidden')
+			menu.classList.remove('flex')
+			burger.classList.remove('active')
+			body.classList.remove('overflow-hidden')
+		}
+	})
+}
+burgerMenu()
+/* SLIDE DOWN */
+let slideDown = (target, duration = 300) => {
+	target.style.removeProperty('display')
+	let display = window.getComputedStyle(target).display
+	if (display === 'none') display = 'grid'
+	target.style.display = display
+	let height = target.offsetHeight
+	target.style.overflow = 'hidden'
+	target.style.height = 0
+	target.style.paddingTop = 0
+	target.style.paddingBottom = 0
+	target.style.marginTop = 0
+	target.style.marginBottom = 0
+	target.offsetHeight
+	target.style.boxSizing = 'border-box'
+	target.style.transitionProperty = 'height, margin, padding'
+	target.style.transitionDuration = duration + 'ms'
+	target.style.height = height + 'px'
+	target.style.border = 'none'
+
+	target.style.removeProperty('padding-top')
+	target.style.removeProperty('padding-bottom')
+	target.style.removeProperty('margin-top')
+	target.style.removeProperty('margin-bottom')
+	target.style.removeProperty('border')
+
+	window.setTimeout(() => {
+		target.style.removeProperty('height')
+		target.style.removeProperty('overflow')
+		target.style.removeProperty('transition-duration')
+		target.style.removeProperty('transition-property')
+		target.style.removeProperty('border')
+	}, duration)
+}
+/* TOOGLE */
+const slideToggle = (target, duration = 300) => {
+	if (window.getComputedStyle(target).display === 'none') {
+		return slideDown(target, duration)
+	} else {
+		return slideUp(target, duration)
+	}
+}
+
+/* AOS */
+
+AOS.init({
+	disable: 'mobile'
+});
